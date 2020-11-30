8ef17da2684c45ba03072232794f5b959dd35383
Justin <justzh2016@gmail.com>
2020-11-29 21:29:28 -0500

    Added 2 methods for intake/outtake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VortechsMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VortechsMethods.java
@@ -2,6 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 class VortechsMethods extends VortechsHardware{
 
+    protected static final double TICKS_PER_INCH = (1120.0/(100.0*Math.PI))*25.4;
+
+    public void launch(double power, long seconds) throws InterruptedException{
+        leftOutTake.setPower(power);
+        rightOutTake.setPower(-power);
+        Thread.sleep(seconds*1000);
+    }
+
+    public void intake(long seconds) throws InterruptedException{
+        intakeWheel.setPower(1);
+        Thread.sleep(seconds*1000);
+    }
+
     
 
 }
