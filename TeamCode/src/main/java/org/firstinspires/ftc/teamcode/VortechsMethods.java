62cc006e89474254fbd0b456c6d8ae8df9994af7
ijax1 <ianshimjackson@gmail.com>
2020-12-03 00:54:44 -0500

    Worked on the PID methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VortechsMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VortechsMethods.java
@@ -1,12 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
 class VortechsMethods extends VortechsHardware {
 
     protected static final double TICKS_PER_INCH = (1120.0 / (100.0 * Math.PI)) * 25.4;
     protected static final double TILE_LENGTH = 24;
+    private final DcMotorEx[] driveMotors = {frontLeft, frontRight, backLeft, backRight};
 
     public void launch(double power, long seconds) throws InterruptedException {
         leftOutTake.setPower(power);
@@ -19,26 +25,57 @@ class VortechsMethods extends VortechsHardware {
         Thread.sleep(seconds * 1000);
     }
 
-    public void move(double forwards) throws InterruptedException {
-        frontRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        frontLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        backLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        double p = 0.0;
-        double i = 0.0;
-        double d = 0.0;
+    public void moveTo(double XTarget, double YTarget) {
+        for(DcMotorEx motor : driveMotors) {
+            motor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+            motor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        }
+        double P = 0.5, I = 0.0, D = 0.0;    //tune the PID here
+        double tolerance = 1.0;
+        double yError, xError;    //the distance from the target
+        double prevYError = 0.0, prevXError = 0.0;
+        ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+        ElapsedTime prevTimer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+        while(opModeIsActive()) {
+            yError = YTarget - YPos;
+            xError = XTarget - XPos;
 
+            double yProportion = P * yError;
+            double xProportion = P * xError;
+
+            double yIntegral =+ I * (yError * (timer.milliseconds() - prevTimer.milliseconds()));
+            double xIntegral =+ I * (xError * (timer.milliseconds() - prevTimer.milliseconds()));
+            
+            double yDerivative = D * (yError - prevYError) / (timer.milliseconds() - prevTimer.milliseconds());
+            double xDerivative = D * (xError - prevXError) / (timer.milliseconds() - prevTimer.milliseconds());
+            
+            prevTimer = timer;
+            double frontRightPower = (P * yProportion + I * yIntegral - D * yDerivative) + 
+                    (P * xProportion + I * xIntegral + D * xDerivative);
+            double frontLeftPower = (P * yProportion + I * yIntegral - D * yDerivative) +
+                    (P * xProportion + I * xIntegral + D * xDerivative);
+                    
+            frontRight.setPower(frontRightPower);
+            backLeft.setPower(frontRightPower);
+
+            frontLeft.setPower(frontLeftPower);
+            backRight.setPower(frontLeftPower);
+        }
     }
 
-    double YPos, XPos;
+    public double ticksToInches(int ticks) {
+        return ticks/TICKS_PER_INCH;
+    }
+
+    public double XPos, YPos, XTarget, YTarget;
+
     public void update() {
-    YPos = frontLeft.getCurrentPosition();
-    XPos = frontRight.getCurrentPosition();
-
+    YPos = ticksToInches(frontLeft.getCurrentPosition());
+    XPos = ticksToInches(frontRight.getCurrentPosition());
     }
+
     public boolean motorsBusy() {
         return frontRight.isBusy() || frontLeft.isBusy() ||
                 backRight.isBusy() || backLeft.isBusy();
     }
-
 }
