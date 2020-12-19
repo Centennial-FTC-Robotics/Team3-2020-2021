9040ee5d141cf8e69be55499fbb8e00a7eef8e7d
ijax1 <ianshimjackson@gmail.com>
2020-12-19 01:13:42 -0500

    Added PID code for turning, more move methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VortechsMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VortechsMethods.java
@@ -4,8 +4,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.PIDCoefficients;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
 class VortechsMethods extends VortechsHardware {
@@ -13,6 +19,8 @@ class VortechsMethods extends VortechsHardware {
     protected static final double TICKS_PER_INCH = (1120.0 / (100.0 * Math.PI)) * 25.4;
     protected static final double TILE_LENGTH = 24;
     private final DcMotorEx[] driveMotors = {frontLeft, frontRight, backLeft, backRight};
+
+    public double XPos, YPos, currentAngle, XTarget, YTarget, angleTarget, initialHeading;
 
     public void launch(double power, long seconds) throws InterruptedException {
         leftOutTake.setPower(power);
@@ -25,57 +33,103 @@ class VortechsMethods extends VortechsHardware {
         Thread.sleep(seconds * 1000);
     }
 
+    public void moveRelative(double sideways, double forward){
+        moveAndTurn(forward - XPos, forward - YPos,0);
+    }
+    public void turnRelative(double degrees) {
+        moveAndTurn(0,0,degrees - currentAngle);
+    }
+    public void turn(double degrees) {
+        moveAndTurn(0,0,degrees);
+    }
     public void moveTo(double XTarget, double YTarget) {
+        moveAndTurn(XTarget, YTarget, 0);
+    }
+    public void moveAndTurn(double XTarget, double YTarget, double angleTarget) {
         for(DcMotorEx motor : driveMotors) {
             motor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
             motor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        }
-        double P = 0.5, I = 0.0, D = 0.0;    //tune the PID here
-        double tolerance = 1.0;
-        double yError, xError;    //the distance from the target
-        double prevYError = 0.0, prevXError = 0.0;
+        }//tune the PID here
+        double P = 0.5;
+        double I = 0.0;
+        double D = 0.0;
+
+        double angleP = 0.5;
+        double angleI = 0.0;
+        double angleD = 0.0;
+
+        double yError, xError, angleError;    //the distance from the target
+        double prevYError = 0.0, prevXError = 0.0, prevAngleError = 0.0;
         ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         ElapsedTime prevTimer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         while(opModeIsActive()) {
             yError = YTarget - YPos;
             xError = XTarget - XPos;
+            angleError = angleTarget - currentAngle;
 
             double yProportion = P * yError;
             double xProportion = P * xError;
+            double aIntegral = angleP * angleError;
 
             double yIntegral =+ I * (yError * (timer.milliseconds() - prevTimer.milliseconds()));
             double xIntegral =+ I * (xError * (timer.milliseconds() - prevTimer.milliseconds()));
+            double aProportion =+ angleI * (angleError * (timer.milliseconds() - prevTimer.milliseconds()));
             
             double yDerivative = D * (yError - prevYError) / (timer.milliseconds() - prevTimer.milliseconds());
             double xDerivative = D * (xError - prevXError) / (timer.milliseconds() - prevTimer.milliseconds());
+            double aDerivative = angleD * (angleError - prevAngleError) / (timer.milliseconds() - prevTimer.milliseconds());
             
             prevTimer = timer;
+            prevYError = yError;
+            prevXError = xError;
+            prevAngleError = angleError;
+
             double frontRightPower = (P * yProportion + I * yIntegral - D * yDerivative) + 
                     (P * xProportion + I * xIntegral + D * xDerivative);
             double frontLeftPower = (P * yProportion + I * yIntegral - D * yDerivative) -
                     (P * xProportion + I * xIntegral + D * xDerivative);
-                    
-            frontRight.setPower(frontRightPower);
-            backLeft.setPower(frontRightPower);
 
-            frontLeft.setPower(frontLeftPower);
-            backRight.setPower(frontLeftPower);
+            double rightTurn = angleP * aProportion + angleI * aIntegral - angleD * aDerivative;
+            double leftTurn = -angleP * aProportion - angleI * aIntegral + angleD * aDerivative;
+
+            frontRight.setPower(frontRightPower + rightTurn);
+            backLeft.setPower(frontRightPower + leftTurn);
+
+            frontLeft.setPower(frontLeftPower + leftTurn);
+            backRight.setPower(frontLeftPower + rightTurn);
         }
     }
-
     public double ticksToInches(int ticks) {
         return ticks/TICKS_PER_INCH;
     }
-
-    public double XPos, YPos, XTarget, YTarget;
-
     public void update() {
     YPos = ticksToInches(frontLeft.getCurrentPosition());
     XPos = ticksToInches(frontRight.getCurrentPosition());
+    orientation = IMU.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+    currentAngle = AngleUnit.normalizeDegrees(orientation.firstAngle - initialHeading);
     }
-
     public boolean motorsBusy() {
         return frontRight.isBusy() || frontLeft.isBusy() ||
                 backRight.isBusy() || backLeft.isBusy();
     }
+    Orientation orientation = new Orientation();
+    public void initializeIMU() {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        IMU.initialize(parameters);
+    }
+    private void resetOrientation(){
+        initialHeading = orientation.firstAngle;
+    }
+    public void setPosition(double XPos, double YPos){
+        this.XPos = XPos;
+        this.YPos = YPos;
+    }
 }
+
