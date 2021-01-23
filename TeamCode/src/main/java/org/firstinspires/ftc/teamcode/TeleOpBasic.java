6ee6c0e080ee37ef36206396997d1c6e44e3a3c8
Justin <justzh2016@gmail.com>
2021-01-22 21:06:18 -0500

    Minor Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpBasic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpBasic.java
@@ -10,6 +10,7 @@ public class TeleOpBasic extends VortechsHardware{
     double Turn = 0.0;
     double RightPower = 0.0;
     double LeftPower = 0.0;
+    double OtherPower = 1.0;
 
     public void runOpMode() throws InterruptedException{
         super.runOpMode();
@@ -20,32 +21,45 @@ public class TeleOpBasic extends VortechsHardware{
         {
             MotorPower = -gamepad1.left_stick_y;
             Steering = gamepad1.left_stick_x;
-            Turn = gamepad2.left_stick_x;
+            Turn = gamepad1.right_stick_x;
             RightPower = MotorPower - Steering;
             LeftPower = MotorPower + Steering;
 
             frontLeft.setPower(LeftPower);
-            backLeft.setPower(LeftPower);
+            backLeft.setPower(RightPower);
             frontRight.setPower(RightPower);
-            backRight.setPower((RightPower));
+            backRight.setPower((LeftPower));
+
+            frontLeft.setPower(MotorPower+Turn);
+            backLeft.setPower(MotorPower+Turn);
+            frontRight.setPower(MotorPower-Turn);
+            backRight.setPower(MotorPower-Turn);
 
             if (this.gamepad1.right_trigger > 0) {    //slowmode
                 RightPower = RightPower/2.0;
                 LeftPower = LeftPower/2.0;
             }
 
-            if (this.gamepad2.left_trigger>1){
-            intakeWheel.setPower(2);
+            if (this.gamepad2.a){
+            intakeWheel.setPower(2*OtherPower);
+            }
+            else{
+            intakeWheel.setPower(0);
             }
 
             conveyor.setPower(this.gamepad2.left_stick_y);
             grabberArm.setPower(this.gamepad2.left_stick_x);
 
-            if (this.gamepad2.right_trigger>1){
-            leftOutTake.setPower(2);
-            rightOutTake.setPower(2);
+            if (this.gamepad2.left_trigger>0){
+            leftOutTake.setPower(OtherPower);
+            rightOutTake.setPower(-OtherPower);
+            }
+            else {
+            leftOutTake.setPower(0.0);
+            rightOutTake.setPower(0.0);
             }
 
+            
             idle();
         }
     }
