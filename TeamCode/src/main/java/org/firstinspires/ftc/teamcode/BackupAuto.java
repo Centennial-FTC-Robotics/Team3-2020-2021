2939a266f4a68d05dc49584271ed80d847f16d33
svalli2264 <shreya.manalan@gmail.com>
2021-01-23 23:09:08 -0500

    added launching to BackupAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackupAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackupAuto.java
@@ -6,11 +6,10 @@ import org.firstinspires.ftc.teamcode.VortechsMethods;
 
 @Autonomous(group = "Autonomous", name = "BackupAuto")
 public class BackupAuto extends VortechsMethods {
-    // robot parks at launch line and
+    // robot parks at launch line and launches rings at high goal
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
-        moveRelative(0, 80); //Park on white line
+        moveRelative(0, 80); //Park on launch line
+        launch(2,3); //Shoot rings
     }
-
-
 }
