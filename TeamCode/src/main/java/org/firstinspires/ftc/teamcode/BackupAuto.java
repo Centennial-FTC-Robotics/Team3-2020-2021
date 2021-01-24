65e83dad38265f52ecc147c05a75e0bb43e25176
svalli2264 <shreya.manalan@gmail.com>
2021-01-23 22:52:12 -0500

    added backup autonomous that parks on launch line

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackupAuto.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.VortechsMethods;
+
+@Autonomous(group = "Autonomous", name = "BlueLeftAuto")
+public class BackupAuto extends VortechsMethods {
+    // robot parks at launch line and
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+        moveRelative(0, 80); //Park on white line
+    }
+
+
+}
