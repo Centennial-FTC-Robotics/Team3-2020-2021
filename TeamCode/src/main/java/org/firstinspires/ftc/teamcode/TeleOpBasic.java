25cc39bc53536763f6b3f9dbb3f15dd3dddf8fd0
Justin <justzh2016@gmail.com>
2021-01-23 19:23:52 -0500

    Changed stuff for TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpBasic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpBasic.java
@@ -5,13 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "Teleop Basic", group = "Tests")
 public class TeleOpBasic extends VortechsHardware{
 
-    double MotorPower = 0.0;
-    double Steering = 0.0;
-    double Turn = 0.0;
-    double TurnThrottle = 0.0;
+    double y = 0.0;   //throttle forward/back
+    double x = 0.0;   //throttle left/right
+    double xr = 0.0;
+    double yr = 0.0;
     double RightPower = 0.0;
     double LeftPower = 0.0;
     double OtherPower = 1.0;
+    boolean intakeOn = false;
 
     public void runOpMode() throws InterruptedException{
         super.runOpMode();
@@ -20,29 +21,58 @@ public class TeleOpBasic extends VortechsHardware{
 
         while(opModeIsActive())
         {
-            MotorPower = -gamepad1.left_stick_y;
-            Steering = gamepad1.left_stick_x;
-            Turn = gamepad1.right_stick_x;
-            TurnThrottle = gamepad1.right_stick_y;
-            RightPower = MotorPower - Steering;
-            LeftPower = MotorPower + Steering;
+            y = -gamepad1.left_stick_y;
+            x = gamepad1.left_stick_x;
+            xr = gamepad1.right_stick_x;
+            yr = gamepad1.right_stick_y;
+            RightPower = y - x;
+            LeftPower = y + x;
 
             frontLeft.setPower(LeftPower);
+            frontRight.setPower(RightPower);
+            backLeft.setPower(LeftPower);
+            backRight.setPower(RightPower);
+
+            if (gamepad1.right_bumper){         //right strafe controlled by right bumper
+            frontLeft.setPower(1);
+            frontRight.setPower(-1);
+            backLeft.setPower(-1);
+            backRight.setPower(1);
+            }
+            if (gamepad1.left_bumper){         //left strafe controlled by left bumper
+            frontLeft.setPower(-1);
+            frontRight.setPower(1);
+            backLeft.setPower(1);
+            backRight.setPower(-1);
+            }
+
+        /*  frontLeft.setPower(LeftPower);         //strafe controlled by left stick
             backLeft.setPower(RightPower);
             frontRight.setPower(RightPower);
-            backRight.setPower((LeftPower));
+            backRight.setPower((LeftPower));  */
 
-            frontLeft.setPower(TurnThrottle+Turn);
-            backLeft.setPower(TurnThrottle+Turn);
-            frontRight.setPower(TurnThrottle-Turn);
-            backRight.setPower(TurnThrottle-Turn);
+        /*    frontLeft.setPower(yr+xr);          //rotation controlled by right stick
+            backLeft.setPower(yr+xr);
+            frontRight.setPower(yr-xr);
+            backRight.setPower(yr-xr);    */
 
-            while (this.gamepad1.right_trigger > 0) {    //slowmode
+            while (gamepad1.right_trigger > 0) {    //slowmode
                 RightPower = RightPower/2.0;
                 LeftPower = LeftPower/2.0;
             }
-            boolean intakeOn = false;
-            if (this.gamepad2.a){
+
+            if (gamepad2.a){
+            intakeOn = !intakeOn;
+             while (intakeOn = true){
+             intakeWheel.setPower(2);
+             }
+            }
+            else {
+            intakeOn = false;
+            }
+            
+         /*   boolean intakeOn = false;
+            if (gamepad2.a){
                 if(intakeOn){
                     intakeOn = false;
                 } else {
@@ -51,12 +81,12 @@ public class TeleOpBasic extends VortechsHardware{
                 }
             } else {
             intakeWheel.setPower(0);
-            } //This is so A doesn't have to be held for the outtake to turn on
+            } //This is so A doesn't have to be held for the outtake to turn on   */
 
-            conveyor.setPower(this.gamepad2.left_stick_y);
-            grabberArm.setPower(this.gamepad2.left_stick_x);
+            conveyor.setPower(gamepad2.left_stick_y);
+            grabberArm.setPower(gamepad2.left_stick_x);
 
-            if (this.gamepad2.left_trigger>0){
+            if (gamepad2.left_trigger>0){
             leftOutTake.setPower(OtherPower);
             rightOutTake.setPower(-OtherPower);
             }
@@ -64,7 +94,7 @@ public class TeleOpBasic extends VortechsHardware{
             leftOutTake.setPower(0.0);
             rightOutTake.setPower(0.0);
             }
-            if (this.gamepad2.right_bumper){
+            if (gamepad2.right_bumper){
             OtherPower = 0.5;
             }
             else{ OtherPower = 1.0;
