87a42b68c71f7289e52f0638abd2c103563f70ca
ijax <ianshimjackson@gmail.com>
2021-01-30 20:40:25 -0500

    added MotorsTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MotorsTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.VortechsMethods;
+@TeleOp
+public class MotorsTest extends VortechsMethods {
+
+        @Override
+        public void runOpMode() throws InterruptedException {
+            super.runOpMode();
+            if(gamepad1.left_bumper)
+                frontLeft.setPower(0.1);
+            if(gamepad1.right_bumper)
+                frontRight.setPower(0.1);
+            if(gamepad1.right_trigger > 0.05)
+                backRight.setPower(0.1);
+            if(gamepad1.left_trigger > 0.05)
+                backLeft.setPower(0.1);
+
+            }
+        }
+
+        
